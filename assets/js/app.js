d55a747b237a00de3ba5192cb6d30fb3713e9c3d
tuanlm1201 <83169461+tuanlm1201@users.noreply.github.com>
2021-11-11 19:48:57 +0700

    update code handle song

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,48 @@
+import arrayMusic from "./listMusic.js";
+
 const openList = document.querySelector('.open-list');
+const closeList = document.querySelector('.close-list');
 const listMusic = document.querySelector('.list-music');
 const playList = document.querySelector('.play-list');
 
 openList.addEventListener('click', handleOpenList);
-
 function handleOpenList() {
-    console.log(listMusic);
     playList.style.bottom = 0 + 'px';
+    openList.style.display = 'none';
+    closeList.style.display = 'flex';
 }
+
+closeList.addEventListener('click', handleCloseList);
+function handleCloseList() {
+    playList.style.bottom = - 300 + 'px';
+    closeList.style.display = 'none';
+    openList.style.display = 'flex';
+}
+/*==================================================================================*/
+//Handle Music
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const app = {
+    songs: arrayMusic,
+    render: function(){
+        const htmls = this.songs.map(song =>{
+            return `
+            <div class="song">
+                <div class="song__thumb" style="background-image: url(${song.img})"></div>
+                <div class="song__body">
+                  <h3 class="song__title">${song.name}</h3>
+                  <p class="song__author">${song.author}</p>
+                </div>
+              </div>
+            `;
+        })
+
+        listMusic.innerHTML = htmls;
+    },
+    start: function(){
+        this.render()
+    }
+}
+app.start();
--- assets/js/listMusic.js
+++ assets/js/listMusic.js
@@ -1,7 +1,40 @@
 let listMusic = [
-    {
-        name: 'Tâm sử tuổi 30',
-        author: 'Trịnh Thanh Bình',
-        src: './'
-    }
+  {
+    name: "Tâm sử tuổi 30",
+    author: "Trịnh Thanh Bình",
+    src: "./assets/music/tamsutuoi30.mp3",
+    img: "./assets/image/tamsutuoi30.jpg",
+  },
+  {
+    name: "Tâm sử tuổi 30",
+    author: "Trịnh Thanh Bình",
+    src: "./assets/music/tamsutuoi30.mp3",
+    img: "./assets/image/tamsutuoi30.jpg",
+  },
+  {
+    name: "Tâm sử tuổi 30",
+    author: "Trịnh Thanh Bình",
+    src: "./assets/music/tamsutuoi30.mp3",
+    img: "./assets/image/tamsutuoi30.jpg",
+  },
+  {
+    name: "Tâm sử tuổi 30",
+    author: "Trịnh Thanh Bình",
+    src: "./assets/music/tamsutuoi30.mp3",
+    img: "./assets/image/tamsutuoi30.jpg",
+  },
+  {
+    name: "Tâm sử tuổi 30",
+    author: "Trịnh Thanh Bình",
+    src: "./assets/music/tamsutuoi30.mp3",
+    img: "./assets/image/tamsutuoi30.jpg",
+  },
+  {
+    name: "Tâm sử tuổi 30",
+    author: "Trịnh Thanh Bình",
+    src: "./assets/music/tamsutuoi30.mp3",
+    img: "./assets/image/tamsutuoi30.jpg",
+  },
 ];
+
+export default listMusic
